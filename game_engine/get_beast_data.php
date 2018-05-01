501b4c24f9439cdcaf500fea2b0d042176845c5e
Christian Kosman <christiankosman@gmail.com>
2018-05-01 11:54:24 -0700

    ready

--- game_engine/get_beast_data.php
+++
@@ -1,5 +0,0 @@
-<?php
-include "../core/init.php";
-
-echo get_beast_data($_GET["username"]);
-?>
---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>Creative Project</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="apple-mobile-web-app-title" content="Fight the Beast">
+	<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="mobile-web-app-capable" content="yes">
+	<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
+	<link rel="apple-touch-icon" href="beast/images/custom_icon.png">
+	<link rel="apple-touch-startup-image" href="beast/images/startup-6.png">
+	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="game_engine/engine.js" type="text/javascript"></script>
+	<script src="game.js" type="text/javascript"></script>
+	<style>
+		body {
+			margin:0;
+			padding:0;
+			font-family: 'Roboto', sans-serif;
+			background-color: #0f0f0f;
+			color:white;
+		}
+		#misclist {
+			list-style-type: none;
+			margin:0;
+			padding:0;
+		}
+		.misclistitem{
+			margin-bottom:10px;
+		}
+		a {
+			text-decoration:none;
+			color:#CCCCCC;
+		}
+	</style>
+</head>
+<body>
+
+</body>
+</html>
